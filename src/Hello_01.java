cf6896d00650f37f7cb30e00b7b943ef399c287e
SANG9623 <abrasax83@naver.com>
2019-05-30 15:48:53 +0900

    first

---
+++ src/Hello_01.java
@@ -0,0 +1,23 @@
+
+public class Hello_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("대한민국만세");
+		System.out.println("00000000");
+		System.out.println(OOOOOOOOOO);
+		System.out.println(oooooooooo);
+		System.out.println(IIIIIIIIII);
+		System.out.println(iiiiiiiiii);
+		System.out.println(llllllllll);
+		
+		
+		boolean bYes =10!= 20;
+		bYes = 10 >= 20;
+		bYes = 10 <= 20;
+		bYes 
+
+	}
+
+}
